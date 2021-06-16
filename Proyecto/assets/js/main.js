ede61c75173c955cb2d3bd3de506e7c66a750fa0
danicoroman <2danicoroman@gmail.com>
2021-06-16 16:52:10 +0200

    arreglo de modo nocturno

--- Proyecto/assets/js/main.js
+++ Proyecto/assets/js/main.js
@@ -1,9 +1,28 @@
+//MODO NOCTURNO WEB
+
 const btnSwitch = document.querySelector('#switch');
+
 btnSwitch.addEventListener('click', () =>  {
+
     document.body.classList.toggle('dark');
+
     btnSwitch.classList.toggle('active');
 
+    //Guardar en localstorage
 
-
-
+    if(document.body.classList.contains('dark')){
+        localStorage.setItem('dark-mode', 'true');
+    }else {
+        localStorage.setItem('dark-mode', 'false');
+    }
 });
+
+//Obtenemos el modo actual.
+
+if(localStorage.getItem('dark-mode') === 'true'){
+    document.body.classList.add('dark');
+    btnSwitch.classList.add('active');
+}else {
+    document.body.classList.remove('dark');
+    btnSwitch.classList.remove('active');
+}
